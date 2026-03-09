0f5e71af74d5b6dac6c0926f04265a5115db99d2
Samantha-2461 <siwelasamantha49@gmail.com>
2026-03-09 15:01:13 +0200

    Update 15practical.java

--- 15practical.java
+++ 15practical.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util;
+import java.io.*;
+import java.util.*;
 
 
 public class StartAnagramsprogram{
