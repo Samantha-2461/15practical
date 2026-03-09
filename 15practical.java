aae2e364bcd988bfbdcdf1f1154fa9dab4d8f49c
Samantha-2461 <siwelasamantha49@gmail.com>
2026-03-09 14:27:39 +0200

    Create 15practical.java

---
+++ 15practical.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util;
+
+
+public class StartAnagramsprogram{
+   public static void main(String args[]) throws FileNotFoundException {
+     Map<String, list> anagramMap = new HashMap<>(); try(BufferedReader file = new BufferedReader (new FileReader (" C:https://ikamva.uwc.ac.za/access/content/attachment/79485e9f-c928-4de4-a7c9-f77365f13fbc/Assignments/8e13757b-d0aa-444c-9223-2f081d572da9/joyce1922_ulysses.text
