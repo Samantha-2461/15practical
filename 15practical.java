291698b30a56ab7b1407b855ac47d03a3ea8cac7
Samantha-2461 <siwelasamantha49@gmail.com>
2026-03-09 14:47:05 +0200

    Update 15practical.java

--- 15practical.java
+++ 15practical.java
@@ -11,5 +11,26 @@ public class StartAnagramsprogram{
       Map<String, List<String>> anagrams = readWords (filename);
       printAnagramGroups(anagrams);
    }
-   
+   public static Map<String, List<String>> readWords (String path) {
+      Map<String, List<String>>map = new HashMap <> ();
+
+      try (BufferedReader reader = new BufferedReader (new FileReader(path))) {
+         String line;
+
+         while ((line = reader.readline()) != null){
+            String [] tokens = line.split ("\\s+");
+            for (String word : tokens) {
+               word = CleanWord(word);
+
+               if (!word.isEmpty()) {
+                  String key = generateKey(word);
+
+                  if( !map.containKey(key)) {
+                     map.put(key, new ArrayList<>());
+                  }
+                  map.get(key).add(word);
+               }
+            }
+         }
+      } catch (IOException e) {
                                                                                                                    
